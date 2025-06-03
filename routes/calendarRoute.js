b64b41401258c00a9ac06d533e921a2bca96f3d9
handagyeong <80438961+handagyeong@users.noreply.github.com>
2025-06-03 15:41:00 +0900

    fix: 경로수정

--- routes/calendarRoute.js
+++ routes/calendarRoute.js
@@ -4,10 +4,10 @@ const calendarController = require('../controllers/calendarController');
 const authMiddleware = require('../middlewares/auth'); // 추가
 
 // calendar 조회 (인증 필요)
-router.get('', authMiddleware, calendarController.getCalendar);
+router.get('/', authMiddleware, calendarController.getCalendar);
 
 // calendar 등록 (인증 필요)
-router.post('', authMiddleware, calendarController.postCalendar);
+router.post('/', authMiddleware, calendarController.postCalendar);
 
 // // calendar 조회 
 // router.get('', calendarController.getCalendar);
