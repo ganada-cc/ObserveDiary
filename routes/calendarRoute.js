de5e48325a1557a5633bee0496289e72a66b3dab
ssohy <sohyeonj415@gmail.com>
2025-06-03 08:14:22 +0000

    fix: 원상복구

--- routes/calendarRoute.js
+++ routes/calendarRoute.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const calendarController = require('../controllers/calendarController');
 // const authMiddleware = require('../middlewares/auth'); // 추가
 
-router.get('/calendar', calendarController.getCalendar);
-router.post('/calendar', calendarController.postCalendar);
+router.get('/', calendarController.getCalendar);
+router.post('/', calendarController.postCalendar);
+
 
 // // calendar 조회 
 // router.get('', calendarController.getCalendar);
