e689defdd1f448d548c106a03d4cec301273139f
ssohy <sohyeonj415@gmail.com>
2025-06-03 06:46:46 +0000

    test: 유저->캘린더 이동 테스트12

--- routes/calendarRoute.js
+++ routes/calendarRoute.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const calendarController = require('../controllers/calendarController');
-const authMiddleware = require('../middlewares/auth'); // 추가
+// const authMiddleware = require('../middlewares/auth'); // 추가
 
-// calendar 조회 (인증 필요)
-router.get('/', authMiddleware, calendarController.getCalendar);
+router.get('/', calendarController.getCalendar);
+router.post('/', calendarController.postCalendar);
 
-// calendar 등록 (인증 필요)
-router.post('/', authMiddleware, calendarController.postCalendar);
 
 // // calendar 조회 
 // router.get('', calendarController.getCalendar);
